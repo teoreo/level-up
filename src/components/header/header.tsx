f94e80b1c398312345901d9b4184cd035cd37f27
[teoreo] <[emma.teo@medieinstitutet.se]>
2020-11-27 16:17:21 +0100

    sass

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import './header.scss'
 
 
 export default function Header(){
     
     return(
-        <div className="Header">Header</div>
+        <div className="Header">
+            <div className="containerH1">
+                <h1>Din <h1 className="orangeTxt">konsult- och rekryteringspartner</h1> där ingen utmaning är för stor</h1>
+            </div>
+        </div>
     )
 }
